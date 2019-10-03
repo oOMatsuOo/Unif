56a9576466e4fd5e799384d229b063811ff10dcd
oOMatsuOo <kieranvm@gmail.com>
2019-10-03 20:09:53 +0200

    Modifications pour challenge 1

--- Projet de progra/Challenge/Challenge1/test.c
+++ Projet de progra/Challenge/Challenge1/test.c
@@ -3,39 +3,39 @@
 int main(){
 
 
-    unsigned int pop_A = 1000000 , pop_B = 34, ans = 0;
-    unsigned int sims_A = pop_A , sims_B = pop_B , annees = ans;
+    unsigned int pop_A = 93 , pop_B = 34, ans = 0;
+    unsigned int population_alpha = pop_A , population_beta = pop_B , annees = ans;
     int augmentation_A = 500000;
     float augmentation_B = 0.03;
 
-    while(sims_B < sims_A){
-        sims_A += augmentation_A;
-        sims_B = sims_B + (sims_B * augmentation_B);
+    while(population_beta < population_alpha){
+        population_alpha = population_alpha+ augmentation_A;
+        population_beta = population_beta + (population_beta * augmentation_B);
         annees ++;
     };
 
     printf("%u\n",annees);
 
-    sims_A = pop_A;
-    sims_B = pop_B;
+    population_alpha = pop_A;
+    population_beta = pop_B;
     annees = ans;
 
-    for( ; sims_B < sims_A; annees++){
-        sims_A += augmentation_A;
-        sims_B = sims_B + (sims_B * augmentation_B);
+    for( ; population_beta < population_alpha; annees++){
+        population_alpha = population_alpha + augmentation_A;
+        population_beta = population_beta + (population_beta * augmentation_B);
     };
 
     printf("%u\n", annees);
 
-    sims_A = pop_A;
-    sims_B = pop_B;
+    population_alpha = pop_A;
+    population_beta = pop_B;
     annees = ans;
 
     do{
-        sims_A += augmentation_A;
-        sims_B = sims_B + (sims_B * augmentation_B);
+        population_alpha = population_alpha + augmentation_A;
+        population_beta = population_beta + (population_beta * augmentation_B);
         annees ++;
-    }while(sims_B < sims_A);
+    }while(population_beta < population_alpha);
 
     printf("%u\n", annees);
 }
