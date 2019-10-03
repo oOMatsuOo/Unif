a7073497acf9c0984d95b90d91eda85a4441a1a8
oOMatsuOo <kieranvm@gmail.com>
2019-10-03 20:23:21 +0200

    Fin challange 1

--- Projet de progra/Challenge/Challenge1/test.c
+++ Projet de progra/Challenge/Challenge1/test.c
@@ -3,12 +3,18 @@
 int main(){
 
 
-    unsigned int pop_A = 93 , pop_B = 34, ans = 0;
+    unsigned int pop_A = 0 , pop_B = 0, ans = 0;
+
+    printf("Insérer la population A\n");
+    scanf("%u",&pop_A);
+    printf("Insérer la population B\n");
+    scanf("%u",&pop_B);
+
     unsigned int population_alpha = pop_A , population_beta = pop_B , annees = ans;
     int augmentation_A = 500000;
     float augmentation_B = 0.03;
 
-    while(population_beta < population_alpha){
+    while(population_beta <= population_alpha){
         population_alpha = population_alpha+ augmentation_A;
         population_beta = population_beta + (population_beta * augmentation_B);
         annees ++;
@@ -20,7 +26,7 @@ int main(){
     population_beta = pop_B;
     annees = ans;
 
-    for( ; population_beta < population_alpha; annees++){
+    for( ; population_beta <= population_alpha; annees++){
         population_alpha = population_alpha + augmentation_A;
         population_beta = population_beta + (population_beta * augmentation_B);
     };
@@ -32,10 +38,12 @@ int main(){
     annees = ans;
 
     do{
-        population_alpha = population_alpha + augmentation_A;
-        population_beta = population_beta + (population_beta * augmentation_B);
-        annees ++;
-    }while(population_beta < population_alpha);
+        if(population_beta <= population_alpha){
+            population_alpha = population_alpha + augmentation_A;
+            population_beta = population_beta + (population_beta * augmentation_B);
+            annees ++;
+        }
+    }while(population_beta <= population_alpha);
 
     printf("%u\n", annees);
 }
