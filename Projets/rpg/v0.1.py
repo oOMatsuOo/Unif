b32d00d51612995bf65f71e6d70f5267eae26c4d
oOMatsuOo <kieranvm@gmail.com>
2019-11-07 00:40:29 +0100

    Ajout rpg

--- Projets/rpg/v0.1.py
+++ Projets/rpg/v0.1.py
@@ -8,62 +8,124 @@ import sys
 BLACK   = (  0,  0,  0)
 RED     = (139,  0,  0)
 PURPLE  = (148,  0,211)
-Grey    = (105,105,105)
+GREY    = (105,105,105)
 ORANGE  = (255,140,  0)
+BLUE    = ( 30,144,237)
+BROWN   = (188,143,143)
 
 #Parameter
 
 game = True
 
-case_size = 40
+screen_size = (1000,1000)
+width = 21
+height= 0
 
-screen_size = (800,600)
-width = 19
+case_size = screen_size[0]//10
 
-height_case = screen_size[1] / case_size
-width_case = screen_size[0] / case_size
-
-framerate = 60
+framerate = 120
 
 field = {}
 
-camera_position = (400, -300)
+camera_position = [1100, 0]
+
+deplacement = case_size // 4
 
 
 #Function
 
+def stay_at_screen():
+    global camera_position
+
+    if camera_position[0] < 0:
+        camera_position[0] = 0
+    
+    if camera_position[1] > 0:
+        camera_position[1] = 0
+    
+    if camera_position[0] > (((width) * case_size) - screen_size[0]):
+        camera_position[0] = (((width) * case_size) - screen_size[0])
+
+    if camera_position[1] < (-((height) * case_size) + screen_size[1]):
+        camera_position[1] = (-((height) * case_size) + screen_size[1])
+
 def manage_keys(key):
+    global camera_position
+
+    if key == pygame.K_LEFT:
+        camera_position[0] -= deplacement
+    elif key == pygame.K_RIGHT:
+        camera_position[0] += deplacement
+    elif key == pygame.K_UP:
+        camera_position[1] += deplacement
+    elif key == pygame.K_DOWN:
+        camera_position[1] -= deplacement
+
     return
 
+def coordinates_to_pixel(coord):
+    
+    coord_px = coord * case_size
+    
+    return(coord_px)
+
+def create_square(x, y):
+
+    p1 = (x,y)
+    p2 = (x+case_size,y)
+    p3 = (x+case_size,y+case_size)
+    p4 = (x,y+case_size)
+
+
+    return(p1, p2, p3, p4)
+
+def define_type_case(type_case):
+    if type_case == 0:
+        color = GREY
+    elif type_case == 1:
+        color = BROWN
+    elif type_case == 2:
+        color = BLUE
+
+    return(color)
+
 def create_case(case_type, x, y):
+    x_coord = coordinates_to_pixel(x)
+    y_coord = coordinates_to_pixel(y)
     
-    
+    square = create_square(x_coord,y_coord)
+
+    color = define_type_case(int(case_type))
 
     pygame.draw.polygon(window, color, square)
     return
 
-def create_screen(camera):
-    Xc, Yc = camera
-    start_case_position = (Xc - screen_size[0] // 2, Yc + screen_size[1] // 2)
+def create_screen(camera_position):
+    height_screen = screen_size[1]//case_size
+    width_screen = screen_size[0]//case_size
 
-    X_start = start_case_position[0] // 40
-    Y_start = start_case_position[1] // 40
+    camY = -camera_position[1]//case_size
+    camX = camera_position[0]//case_size
 
-    i = 0
-    while i < height_case:
-        j = 0
-        while j < width_case:
-            #print(field[X_start + i])
-            print(j)
+    #print(str(camX) + "  " +  str(camY))
+
+    i = camY
+    i_prime = 0
+    while i < height_screen + camY :
+        j = camX
+        j_prime = 0
+        while j < width_screen + camX:
+            create_case(field[i][j], j_prime, i_prime)
             j += 1
-        print(i)
+            j_prime += 1
+
         i += 1
-    
+        i_prime += 1
 
     return
 
 def create_field(width):
-    global field
+    global field, height
 
     case = {}
 
@@ -73,17 +135,19 @@ def create_field(width):
 
         for row in csv_reader:
             case_count = 0
+            case = {}
 
             while case_count < width:
-                case["id"] = case_count
-                case["type"] = row[case_count]
+                print(case_count)
+                case[case_count] = row[case_count]
                 case_count += 1
             
             field[line_count] = case
             line_count += 1
         print(f"Processed {line_count} lines")
-        print(f"Processed {case_count + 1} cases")
+        print(f"Processed {case_count} cases")
 
+        height = (line_count)
 
     return
 
@@ -101,8 +165,6 @@ pygame.key.set_repeat(10,10)
 
 create_field(width)
 
-create_screen(camera_position)
-
 while game:
     for evenement in pygame.event.get():
         if evenement.type == pygame.QUIT:
@@ -111,7 +173,9 @@ while game:
         elif evenement.type == pygame.KEYDOWN:
             manage_keys(evenement.key)
 
-    window.fill(background_color)
+    stay_at_screen()
+
+    create_screen(camera_position)
 
     pygame.display.flip()
 
